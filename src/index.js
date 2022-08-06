467ce29303704801d28ae6ddb621cb9289ea493d
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-08-06 21:55:56 +0500

    init settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {theme} from "./utils/theme";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <App />
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 )
